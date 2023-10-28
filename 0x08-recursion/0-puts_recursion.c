fb9980ad2c5271a393a7adb2b3ed3813bb0f0495
Kamal-Deen Abdulmumin <kamalabdulmumin0@gmail.com>
2023-10-28 08:46:34 +0100

    Adding my 0x08-recursion folder for the test

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+/**
+ * adding main.h file in order to 
+ * access the function made
+ *
+ * void means the function returns nothing
+ *
+ */
+
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+/**
+*main function is the entering point of my code
+
