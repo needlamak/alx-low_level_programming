1389094e3184abb4ca75963b2b044e1820b9d050
Kamal-Deen Abdulmumin <kamalabdulmumin0@gmail.com>
2023-10-31 07:41:39 +0100

    Adding 0-puts again after putting #include "main.h" first

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,14 @@
+#include "main.h"
 /**
  * adding main.h file in order to 
  * access the function made
  *
  * void means the function returns nothing
  *
- */
-
-#include "main.h"
-
-/**
  * _puts_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
